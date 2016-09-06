f78fb152591abc394bc0dda21d710398148ca79c
Brian Ballantine <brian@kontor.com>
2016-09-06 15:58:34 -0400

    not sure what this is --- commit

--- src/sweeded/slides.cljs
+++ src/sweeded/slides.cljs
@@ -2,73 +2,72 @@
 
 (def slides [
 {
-:title "How do software engineers collaborate?"
+:title "Kontor/Forge"
 :body
 "
-* ad-hoc conversations, whiteboarding
-* meetings that start and end on time
-* slack, hipchat, etc
-* paired programming
-* code reviews
+![Cubes](/image/cubes.gif 'Cubes')
+###Re-make Your Workspace###
 "}
 {
-:title "Github"
+:title "New Projects with One Click Technology"
 :body
 "
-###Where software is built###
+<img src='/image/project.png' style='width: 700px'/>
 
-Powerful collaboration, code review, and code management for
-open source and private projects. Public projects are always free. 
-Private plans start at $7/mo.
+* Start a blank project and let your imagination go wild
+* Or Pick from one of our designer templates because you're just too damn busy
 "}
 {
-:title "Github in the World"
+:title "Get matched with a world-class designer"
 :body
 "
-* 10 million users
-* 415 employees
-* In July 2015 GitHub announced it had raised $250 million in funding in a round led by Sequoia Capital
-* The round valued the company at approximately $2 billion
+<img src='/image/designer.png' style='width: 700px'/>
+
+* Choose a designer in your area and book a free e-consultation
+* Sometimes you need professional help
 "}
 {
-:title "Quick look at Git"
+:title "Manage your project with the Forge Board"
 :body
 "
-* Command line tool
-* Keeps track of changes
-* Branching, merging change paths
+<img src='/image/manage.png' style='width: 700px'/>
+
+* Keep up to date with changes on the _Project Feed_
+* Upload and share _Floorplans_
+* Collaborate and get inspired with _Mood Boards_
+* Manage your project _Team_
+* Keep track of the _Materials_ you need to complete your project
+* Create and track _Orders_
+* Stay on _Budget_
 "}
 {
-:title "Things Github does"
+:title "What Others Have Said"
 :body
 "
-* Web interface for Git
-* Viewing code and diffs
-* Documentation
-* Issue tracking
-* Wikis
-* Pull Requests & Code Reviews
-* Email notifications
-* Host small websites
-* And more
-"}
-{
-:title "How we use Github"
-:body
-"
-* Anatomy of a change
-    * Branch
-    * Make local changes
-    * Push changes
-    * Open up PR
-    * Get feedback
-    * Merge changes
-"}
-{
-:title "A Code Review"
-:body
-"
-https://github.com/kontor-labs/gropius/pull/643
+![Cubes](/image/trees.gif 'Cubes')
+
+*Kontor Forge connected us to our new favorite designer, James Coates. I gave him the Floorplans and a couple of mood boards, and he came back with some amazing ideas.*
+
+-Shawn Stone, GM Robinson Concierge
+
+****
+
+*My boss tasked me, a lowly computer programmer, to fit five more interns into our small office and gave me a tiny budget (that bastard!). Luckily I was able to use Kontor Forge to manage the project. I got it done on time and on budget and my boss gave me the rest of the year off without pay! Win!*
+
+-Ryan Valentine, Lowly Programmer, Penetrode, Inc.
+
+****
+
+*When we pivoted our compression network business to video chat, we had to scale up fast, but we didn't want to lose our identity. The designers from Kontor Forge helped guide the way and the collaboration tools allowed everyone to have their say. Our new office is fantastic, thanks to Kontor!*
+
+-Erlich Bachman, Majority Investor, Pied Piper
+
+****
+
+*I wanted manage the expansion of Blue Book's AI division from my isolated Nordic retreat. Kontor Forge helped me run the project, find a great human designer, communicate with my team, and acquire all materials I needed right through their online dashboard, giving me more time to pump iron and drink beer. Amazing Kontor!*
+
+-Nathan Bateman, CEO, Blue Book
+
 "}
 ])
 
